f8674c5f0d978cecf2b0fcdb7b53efc411a97a59
Patriskem <patrikschembri@gmail.com>
2023-05-22 20:07:30 +0200

    prikaze se kamera in mogoce se tudi shrani slika

--- app/src/main/java/si/uni_lj/fe/tnuv/snapmap/MainActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/snapmap/MainActivity.java
@@ -19,11 +19,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ugibaj = findViewById(R.id.lokacija_gumb);
         ugibaj.setOnClickListener(this);
 
+        //ImageView slikaj = findViewById(R.id.lokacija_gumb2);
+        //slikaj.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(this, Lokacija.class);
+        Intent intent = new Intent(this, Slikaj.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/si/uni_lj/fe/tnuv/snapmap/Slikaj.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/snapmap/Slikaj.java
@@ -1,4 +1,162 @@
 package si.uni_lj.fe.tnuv.snapmap;
 
-public class Slikaj {
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Slikaj extends AppCompatActivity implements SurfaceHolder.Callback {
+    private static final int REQUEST_CAMERA_PERMISSION = 200;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private Camera camera;
+    private SurfaceHolder surfaceHolder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.slikaj);
+
+        SurfaceView surfaceView = findViewById(R.id.cameraPreview);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+    }
+
+    public void capturePicture(View view) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+        } else {
+            if (camera != null) {
+                camera.takePicture(null, null, pictureCallback);
+            }
+        }
+    }
+
+    private Camera.PictureCallback pictureCallback = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Bitmap imageBitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            saveImageLocally(imageBitmap);
+            camera.startPreview();
+        }
+    };
+
+    private void saveImageLocally(Bitmap imageBitmap) {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "IMG_" + timeStamp + ".jpg";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imageFile = new File(storageDir, imageFileName);
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Add the image to the system's Media Store (optional)
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri contentUri = Uri.fromFile(imageFile);
+        mediaScanIntent.setData(contentUri);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        if (holder.getSurface() == null) {
+            return;
+        }
+
+        try {
+            camera.stopPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Release the camera resources
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
+    }
+
+    private boolean isCameraInitialized = false;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initializeCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
+    }
+
+    private void initializeCamera() {
+        if (camera == null) {
+            try {
+                camera = Camera.open();
+                isCameraInitialized = true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void releaseCamera() {
+        if (camera != null) {
+            if (isCameraInitialized) {
+                camera.stopPreview();
+                isCameraInitialized = false;
+            }
+            camera.release();
+            camera = null;
+        }
+    }
 }
