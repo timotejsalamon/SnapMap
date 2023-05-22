57ce638c4cc44eace69955830632bdefff8aa3df
timotejsalamon <ts2136@student.uni-lj.si>
2023-05-22 13:38:10 +0200

    gumb lokacije

--- app/src/main/java/si/uni_lj/fe/tnuv/snapmap/Lokacija.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/snapmap/Lokacija.java
@@ -1,4 +1,14 @@
 package si.uni_lj.fe.tnuv.snapmap;
 
-public class Lokacija {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Lokacija extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lokacija);
+    }
 }
--- app/src/main/java/si/uni_lj/fe/tnuv/snapmap/MainActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/snapmap/MainActivity.java
@@ -2,13 +2,28 @@ package si.uni_lj.fe.tnuv.snapmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView ugibaj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ugibaj = findViewById(R.id.lokacija_gumb);
+        ugibaj.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, Lokacija.class);
+        startActivity(intent);
     }
 }
